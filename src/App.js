4516c4bba698afbf81df80d5e5a1be1b4c65dcb6
VictorMonk <107959125+VictorMonk@users.noreply.github.com>
2024-05-30 00:54:27 -0400

    Add header Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import StickyNotesApp from './StickyNotesApp';
+import Header from './Header';
 import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <StickyNotesApp />
       <Footer />
     </div>
